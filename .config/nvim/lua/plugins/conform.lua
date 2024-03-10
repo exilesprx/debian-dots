a156f999265ade5f2e50355a0635fcf6c3e6642d
Exiles.prx <exiles.prx@gmail.com>
2024-03-10 18:48:07 -0400

    Updates from arch dots

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,39 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  opts = {
+    ---@type table<string, conform.FormatterUnit[]>
+    formatters_by_ft = {
+      ["css"] = { "prettier" },
+      ["html"] = { "prettier" },
+      ["javascript"] = { "prettier" },
+      ["json"] = { "prettier" },
+      ["lua"] = { "stylua" },
+      ["markdown"] = { "prettier" },
+      ["php"] = { "phpcs" },
+      ["python"] = { "python" },
+      ["typescript"] = { "prettier" },
+      ["sh"] = { "shfmt" },
+      ["vue"] = { "prettier" },
+      ["yaml"] = { "prettier" },
+    },
+    -- The options you set here will be merged with the builtin formatters.
+    -- You can also define any custom formatters here.
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      phpcs = {
+        args = {
+          "--rules=@PSR12,ordered_imports",
+          "--no-interaction",
+          "fix",
+          "$FILENAME",
+        },
+        command = "php-cs-fixer",
+        filetypes = { "php" },
+        stdin = false,
+      },
+    },
+  },
+}
