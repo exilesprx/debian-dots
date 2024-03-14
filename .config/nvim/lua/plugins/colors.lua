4cbebf344f4c7c0c4df7314e4954e46f6e897d4a
Exiles.prx <exiles.prx@gmail.com>
2024-03-13 22:00:28 -0400

    Catppuccin theme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "catppuccin",
+    priority = 1,
+    opts = {
+      transparent_background = true,
+      flavour = "macchiato",
+    },
+  },
+  {
     "navarasu/onedark.nvim",
     opts = {
       transparent = true,
