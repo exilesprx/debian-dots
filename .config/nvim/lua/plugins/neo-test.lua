4fcbb0364bcf48da164550ebc0ce90d4db19e38f
Exiles.prx <exiles.prx@gmail.com>
2024-05-20 19:45:19 -0400

    Remove plenary
    Its causing conflicts with python and doesn't let the tests run

--- .config/nvim/lua/plugins/neo-test.lua
+++ .config/nvim/lua/plugins/neo-test.lua
@@ -4,17 +4,14 @@ return {
     optional = true,
     dependencies = {
       "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       "lawrence-laz/neotest-zig",
       "mrcjkb/neotest-haskell",
       "nvim-neotest/neotest-python",
-      "nvim-neotest/neotest-plenary",
     },
     opts = {
       adapters = {
-        "neotest-plenary",
         "neotest-zig",
         "neotest-haskell",
         ["neotest-python"] = {
