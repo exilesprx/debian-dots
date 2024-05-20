b8ddc5e1b6faf7628850c6ee8035d2b3ad6d4ebe
Exiles.prx <exiles.prx@gmail.com>
2024-05-20 19:35:43 -0400

    Updates for neotest

--- .config/nvim/lua/plugins/neo-test.lua
+++ .config/nvim/lua/plugins/neo-test.lua
@@ -1,12 +1,16 @@
 return {
   {
     "nvim-neotest/neotest",
+    optional = true,
     dependencies = {
-      "nvim-neotest/neotest-plenary",
       "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
       "lawrence-laz/neotest-zig",
       "mrcjkb/neotest-haskell",
       "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-plenary",
     },
     opts = {
       adapters = {
