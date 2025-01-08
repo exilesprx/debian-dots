69979ec424c84f99cc2bf946a0dbf26b1dcadfbe
Exiles.prx <exiles.prx@gmail.com>
2025-01-08 17:03:21 -0500

    Update oil options

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,12 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
   },
 }
